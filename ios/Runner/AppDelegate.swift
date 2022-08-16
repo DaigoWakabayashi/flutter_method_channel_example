204be04bdf77ef6467deb991ab04827f8ea5b55a
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-08-16 17:43:00 +0900

    :shirt: format swift code
    # :bug: バグ修正
    # :+1: 機能改善
    # :sparkles: 部分的な機能追加
    # :art: デザイン変更のみ
    # :anger: コンフリクト
    # :construction: WIP
    # :memo: 文言修正
    # :recycle: リファクタリング
    # :fire: 不要な機能・使われなくなった機能の削除
    # :green_heart: テストやCIの修正・改善
    # :shirt: Lintエラーの修正やコードスタイルの修正
    # :rocket: パフォーマンス改善
    # :up: 依存パッケージなどのアップデート
    # :cop: セキ ュリティ関連の改善
    # :gear: config変更
    # :books: ドキュメント

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,39 +3,38 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-        // Flutter で定義した MethodChennel を定義
-    let batteryChannel = FlutterMethodChannel(name: "method.channel.app/battery",
-                                              binaryMessenger: controller.binaryMessenger)
-        batteryChannel.setMethodCallHandler({
-          [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-          // This method is invoked on the UI thread.
-          guard call.method == "getBatteryLevel" else {
-            result(FlutterMethodNotImplemented)
-            return
-          }
-          self?.receiveBatteryLevel(result: result)
-        })
-
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            
+            let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+            // Flutter で定義した MethodChannel を定義
+            let batteryChannel = FlutterMethodChannel(name: "method.channel.app/battery",
+                                                      binaryMessenger: controller.binaryMessenger)
+            batteryChannel.setMethodCallHandler({
+                [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+                guard call.method == "getBatteryLevel" else {
+                    result(FlutterMethodNotImplemented)
+                    return
+                }
+                self?.receiveBatteryLevel(result: result)
+            })
+            
+            GeneratedPluginRegistrant.register(with: self)
+            return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        }
     
     // バッテリー残量を取得するメソッド
     private func receiveBatteryLevel(result: FlutterResult) {
-      let device = UIDevice.current
-      device.isBatteryMonitoringEnabled = true
-      if device.batteryState == UIDevice.BatteryState.unknown {
-        result(FlutterError(code: "UNAVAILABLE",
-                            message: "Battery level not available.",
-                            details: nil))
-      } else {
-        result(Int(device.batteryLevel * 100))
-      }
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        if device.batteryState == UIDevice.BatteryState.unknown {
+            result(FlutterError(code: "UNAVAILABLE",
+                                message: "バッテリー残量が取得できません",
+                                details: nil))
+        } else {
+            result(Int(device.batteryLevel * 100))
+        }
     }
 }
 
